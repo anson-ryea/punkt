2ce41dbd9e7ca04d2d26ba70304d3b362775cc68
Anson Ng <914anson5@gmail.com>
2025-10-05 15:59:14 +0800

    [feat] enhance sync command to support multiple targets and include/exclude regex options

--- src/main/kotlin/command/Sync.kt
+++ src/main/kotlin/command/Sync.kt
@@ -1,30 +1,48 @@
 package com.an5on.command
 
-import com.an5on.operation.ManageState.sync
+import com.an5on.operation.StateOperations.sync
 import com.an5on.operation.SyncOptions
+import com.an5on.states.tracked.TrackedEntriesStore
 import com.an5on.utils.FileUtils.replaceTildeWithAbsPathname
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.convert
+import com.github.ajalt.clikt.parameters.arguments.multiple
+import com.github.ajalt.clikt.parameters.arguments.unique
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.file
 import io.github.oshai.kotlinlogging.KotlinLogging
-import java.io.File
 
 private val logger = KotlinLogging.logger {}
 
 class Sync: CliktCommand() {
     val recursive by option("-r", "--recursive", help="Sync directories recursively").flag(default = true)
+    val include by option("-i", "--include", help="Include files matching the regex pattern")
     val exclude by option("-x", "--exclude", help="Exclude files matching the regex pattern")
-    val target by argument()
+    val targets by argument().convert {
+        replaceTildeWithAbsPathname(it)
+    }.file(
+        canBeFile = true,
+        canBeDir = true,
+        canBeSymlink = true,
+        mustExist = true,
+        mustBeReadable = true
+    ).multiple().unique()
 
     override fun run() {
         val options = SyncOptions(
             recursive,
+            include?.toRegex() ?: Regex(".*"), // Matches everything if include is null
             exclude?.toRegex() ?: Regex("$^") // Matches nothing if exclude is null
         )
 
-        val targetFile = File(replaceTildeWithAbsPathname(target))
+        TrackedEntriesStore.connect()
 
-        sync(targetFile, options, ::echo)
+        targets.forEach {
+            sync(it, options, ::echo)
+        }
+
+        TrackedEntriesStore.disconnect()
     }
 }
--- src/main/kotlin/operation/SyncOptions.kt
+++ src/main/kotlin/operation/SyncOptions.kt
@@ -2,5 +2,6 @@ package com.an5on.operation
 
 class SyncOptions (
     val recursive: Boolean,
+    val include: Regex,
     val exclude: Regex
 )
