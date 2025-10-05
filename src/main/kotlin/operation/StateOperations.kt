0bfff55af866846ac1b60ee0010a5ed4abc2a615
Anson Ng <914anson5@gmail.com>
2025-10-05 15:56:25 +0800

    [refactor] rename ManageState to StateOperations

---
+++ src/main/kotlin/operation/StateOperations.kt
@@ -0,0 +1,89 @@
+package com.an5on.operation
+
+import arrow.core.Either
+import arrow.core.raise.either
+import com.an5on.config.ActiveConfiguration
+import com.an5on.states.local.LocalState
+import com.an5on.states.local.LocalState.getLocalFile
+import com.an5on.states.tracked.TrackedEntriesStore
+import com.an5on.states.tracked.TrackedEntryDir
+import com.an5on.states.tracked.TrackedEntryFile
+import com.an5on.utils.CliktEcho
+import com.an5on.utils.FileUtils.getBlake3HashHexString
+import com.github.ajalt.clikt.core.CliktError
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.attribute.DosFileAttributes
+import kotlin.io.path.pathString
+
+
+object StateOperations {
+    fun sync(activeFile: File, options: SyncOptions, echo: CliktEcho): Either<CliktError, Unit> = either {
+
+        var filteredActiveFiles = if (options.recursive) {
+            activeFile.walk().filter { !it.path.matches(options.exclude) }.filter { it.path.matches(options.include) }
+        } else {
+            when (activeFile.isDirectory) {
+                true -> activeFile.listFiles()!!.toList()
+                false -> listOf(activeFile)
+            }.filter { !it.path.matches(options.exclude) }.filter { it.path.matches(options.include) }.asSequence()
+        }
+
+        if (filteredActiveFiles.any { it.path == ActiveConfiguration.localDirAbsPathname }) {
+            echo(
+                "Directories and files in the local Punkt directory (${ActiveConfiguration.localDirAbsPathname}) will not be synced.",
+                true,
+                false
+            )
+            filteredActiveFiles =
+                filteredActiveFiles.filter { !it.path.startsWith(ActiveConfiguration.localDirAbsPathname) }
+        }
+
+        for (activeFile in filteredActiveFiles) {
+            val relativePath = activeFile.relativeTo(File(ActiveConfiguration.homeDirAbsPath.pathString)).toPath()
+            val localFile = getLocalFile(relativePath)
+
+            if (localFile.exists()) {
+                when (activeFile.isDirectory) {
+                    true -> continue
+                    false -> {
+                        val trackedEntryFile = TrackedEntriesStore[activeFile.toPath()] as TrackedEntryFile?
+                        if (trackedEntryFile != null && (activeFile.lastModified() == trackedEntryFile.activeLastModified
+                                    || getBlake3HashHexString(activeFile) == trackedEntryFile.contentHash)
+                        ) {
+                            continue
+                        }
+                    }
+                }
+            }
+
+            if (activeFile.isDirectory) {
+                TrackedEntriesStore[activeFile.toPath()] = TrackedEntryDir()
+
+                if (!localFile.exists()) {
+                    localFile.mkdirs()
+                    echo("Created directory: ${localFile.path}", true, false)
+                }
+            } else {
+                if (!localFile.parentFile.exists()) {
+                    localFile.parentFile.mkdirs()
+                    echo("Created directory: ${localFile.parentFile.path}", true, false)
+                }
+
+                if (!localFile.exists() || localFile.readText() != activeFile.readText()) {
+                    val activeFileBasicAttributes =
+                        Files.readAttributes(activeFile.toPath(), BasicFileAttributes::class.java)
+                    Files.readAttributes(activeFile.toPath(), DosFileAttributes::class.java)
+                    TrackedEntriesStore[activeFile.toPath()] = TrackedEntryFile(
+                        activeFileBasicAttributes.lastModifiedTime().toMillis(),
+                        getBlake3HashHexString(activeFile)
+                    )
+
+                    LocalState.copyFileFromActiveToLocal(activeFile.toPath())
+                    echo("Synced file: ${localFile.path}", true, false)
+                }
+            }
+        }
+    }
+}
