6a06bff640ad095cba10d2dde31b36fac698a65b
Anson Ng <914anson5@gmail.com>
2025-10-05 15:59:46 +0800

    [feat] add TrackedError interface

---
+++ src/main/kotlin/error/TrackedError.kt
@@ -0,0 +1,16 @@
+package com.an5on.error
+
+import java.nio.file.Path
+
+interface TrackedError: PunktError {
+    override val code: String
+
+    data class ConnectFailed(
+        val path: Path,
+        override val cause: Throwable? = null
+    ) : TrackedError {
+        override val code: String = "TRACKED_CONNECT_FAILED"
+        override val message: String
+            get() = "Failed to connect to tracked state database: $path"
+    }
+}
