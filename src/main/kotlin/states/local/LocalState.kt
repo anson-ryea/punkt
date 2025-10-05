7a36d38998862eadb807e775c2875deaeb98f26b
Anson Ng <914anson5@gmail.com>
2025-10-05 15:58:44 +0800

    [feat] make dotfile regex to be OS-specific and replace existing files during copy

--- src/main/kotlin/states/local/LocalState.kt
+++ src/main/kotlin/states/local/LocalState.kt
@@ -1,21 +1,31 @@
 package com.an5on.states.local
 
 import com.an5on.config.ActiveConfiguration
+import com.an5on.config.Configuration
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.StandardCopyOption
 import kotlin.io.path.Path
 import kotlin.io.path.pathString
 import kotlin.io.path.relativeTo
+import kotlin.text.contains
 
 object LocalState {
+    val dotFileRegex = when {
+        Configuration.osName.contains("windows", ignoreCase = true) -> Regex("^\\.(?!\\\\)|(?<=\\\\)\\.")
+        else -> Regex("^\\.(?!/)|(?<=/)\\.")
+    }
+
     fun copyFileFromActiveToLocal(activePath: Path) {
         val activeAbsPath = activePath.toAbsolutePath()
 
-        Files.copy(activeAbsPath, getLocalAbsPath(activeAbsPath.relativeTo(ActiveConfiguration.homeDirAbsPath)))
+        Files.copy(activeAbsPath, getLocalAbsPath(activeAbsPath.relativeTo(ActiveConfiguration.homeDirAbsPath)), StandardCopyOption.REPLACE_EXISTING)
     }
 
-    fun getLocalRelPathname(relPathname: String): String = relPathname.replace(Regex("^\\.(?!/)|(?<=/)\\."), ActiveConfiguration.dotReplacementString)
+    fun getLocalRelPathname(relPathname: String): String = relPathname.replace(
+        dotFileRegex,
+        ActiveConfiguration.dotReplacementString)
 
     fun getLocalAbsPath(relPathname: String): Path = Path(ActiveConfiguration.localDirAbsPathname + "/" + getLocalRelPathname(relPathname))
 
