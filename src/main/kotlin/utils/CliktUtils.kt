f1763052e748d8ae78d0ff25f22cf69b13670f66
Anson Ng <914anson5@gmail.com>
2025-10-06 10:49:08 +0800

    [refactor] add Echo class for passing echo functions

--- src/main/kotlin/utils/CliktUtils.kt
+++ src/main/kotlin/utils/CliktUtils.kt
@@ -1,3 +1,11 @@
 package com.an5on.utils
 
+import com.github.ajalt.clikt.core.CliktCommand
+
 typealias CliktEcho = (Any?, Boolean, Boolean) -> Unit
+
+fun CliktCommand.echoStage(message: Any?) = this.echo("\u001B[1m\u001B[34m~~>\u001B[39m $message\u001B[0m", err = false)
+
+fun CliktCommand.echoSuccess(message: Any? = "Done!") = this.echo("\u001B[32m :>\u001B[39m $message\u001B[0m", err = false)
+
+fun CliktCommand.echoWarning(message: Any?) = this.echo("\u001B[1m\u001B[33m :| $message\u001B[0m", err = false)
