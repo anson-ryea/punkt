37455082cc5c8969d003f7f8c3ec33a4517d8a38
Anson Ng <914anson5@gmail.com>
2025-10-06 10:51:39 +0800

    [refactor] refactor error handling in StateOperations

--- src/main/kotlin/operation/StateOperations.kt
+++ src/main/kotlin/operation/StateOperations.kt
@@ -2,14 +2,22 @@ package com.an5on.operation
 
 import arrow.core.Either
 import arrow.core.raise.either
+import arrow.core.raise.ensure
 import com.an5on.config.ActiveConfiguration
+import com.an5on.error.GitError
+import com.an5on.error.LocalError
+import com.an5on.error.PunktError
 import com.an5on.states.local.LocalState
 import com.an5on.states.local.LocalState.getActiveFile
 import com.an5on.states.local.LocalState.getLocalFile
+import com.an5on.states.local.LocalTransaction
+import com.an5on.states.local.LocalTransactionCopyToLocal
+import com.an5on.states.local.LocalTransactionMakeDirectories
 import com.an5on.states.tracked.TrackedEntriesStore
 import com.an5on.states.tracked.TrackedEntryDir
 import com.an5on.states.tracked.TrackedEntryFile
 import com.an5on.utils.CliktEcho
+import com.an5on.utils.Echos
 import com.an5on.utils.FileUtils.getBlake3HashHexString
 import com.github.ajalt.clikt.core.CliktError
 import java.io.File
@@ -21,9 +29,12 @@ import kotlin.io.path.pathString
 import kotlin.io.path.relativeTo
 import kotlin.io.path.walk
 
-
 object StateOperations {
-    fun sync(activeFile: File, options: SyncOptions, echo: CliktEcho): Either<CliktError, Unit> = either {
+    fun sync(activeFile: File, options: SyncOptions, echo: Echos): Either<PunktError, Unit> = either {
+
+        ensure(LocalState.checkLocalExists()) {
+            LocalError.LocalNotFound()
+        }
 
         var filteredActiveFiles = if (options.recursive) {
             activeFile.walk().filter { !it.path.matches(options.exclude) }.filter { it.path.matches(options.include) }
@@ -35,14 +46,14 @@ object StateOperations {
         }
 
         if (filteredActiveFiles.any { it.path == ActiveConfiguration.localDirAbsPathname }) {
-            echo(
-                "Directories and files in the local Punkt directory (${ActiveConfiguration.localDirAbsPathname}) will not be synced.",
-                true,
-                false
+            echo.echoWarning(
+                "Directories and files in the local Punkt directory (${ActiveConfiguration.localDirAbsPathname}) will not be synced."
             )
             filteredActiveFiles =
                 filteredActiveFiles.filter { !it.path.startsWith(ActiveConfiguration.localDirAbsPathname) }
         }
+
+        val localTransactions = mutableListOf<LocalTransaction>()
 
         for (activeFile in filteredActiveFiles) {
             val relativePath = activeFile.relativeTo(File(ActiveConfiguration.homeDirAbsPath.pathString)).toPath()
@@ -53,8 +64,8 @@ object StateOperations {
                     true -> continue
                     false -> {
                         val trackedEntryFile = TrackedEntriesStore[activeFile.toPath()] as TrackedEntryFile?
-                        if (trackedEntryFile != null && (
-                                    activeFile.lastModified() == localFile.lastModified() &&
+                        if (trackedEntryFile != null &&
+                            (activeFile.lastModified() == localFile.lastModified() &&
                                             getBlake3HashHexString(activeFile) == getBlake3HashHexString(localFile))
                         ) {
                             continue
@@ -67,32 +78,39 @@ object StateOperations {
                 TrackedEntriesStore[activeFile.toPath()] = TrackedEntryDir()
 
                 if (!localFile.exists()) {
-                    localFile.mkdirs()
-                    echo("Created directory: ${localFile.path}", true, false)
+                    localTransactions.add(
+                        LocalTransactionMakeDirectories(relativePath)
+                    )
                 }
             } else {
                 if (!localFile.parentFile.exists()) {
-                    localFile.parentFile.mkdirs()
-                    echo("Created directory: ${localFile.parentFile.path}", true, false)
+                    localTransactions.add(
+                        LocalTransactionMakeDirectories(relativePath)
+                    )
                 }
 
                 if (!localFile.exists() || localFile.readText() != activeFile.readText()) {
                     val activeFileBasicAttributes =
                         Files.readAttributes(activeFile.toPath(), BasicFileAttributes::class.java)
-                    Files.readAttributes(activeFile.toPath(), DosFileAttributes::class.java)
+
                     TrackedEntriesStore[activeFile.toPath()] = TrackedEntryFile(
                         activeFileBasicAttributes.lastModifiedTime().toMillis(),
                         getBlake3HashHexString(activeFile)
                     )
 
-                    LocalState.copyFileFromActiveToLocal(activeFile.toPath())
-                    echo("Synced file: ${localFile.path}", true, false)
+                    localTransactions.add(
+                        LocalTransactionCopyToLocal(activeFile.toPath())
+                    )
                 }
             }
         }
+
+        localTransactions.forEach {
+            it.run().bind()
+        }
     }
 
-    fun syncExistingLocal(options: SyncOptions, echo: CliktEcho): Either<CliktError, Unit> = either {
+    fun syncExistingLocal(options: SyncOptions, echo: Echos): Either<PunktError, Unit> = either {
         val activeFilesInLocal = ActiveConfiguration.localDirAbsPath
             .walk(PathWalkOption.BREADTH_FIRST)
             .filter { it != ActiveConfiguration.localDirAbsPath }
