b4f7c14e1c9f1e1b7a52c42bb9e87a62acbbadd3
Anson Ng <914anson5@gmail.com>
2025-10-07 23:28:21 +0800

    [feat] add command list (#16)
    * [feat] add PathStyles enum for path type categorization
    
    * [feat] implement List command

--- src/main/kotlin/operation/Operations.kt
+++ src/main/kotlin/operation/Operations.kt
@@ -4,20 +4,20 @@ import arrow.core.Either
 import arrow.core.raise.either
 import arrow.core.raise.ensure
 import com.an5on.config.ActiveConfiguration
+import com.an5on.config.ActiveConfiguration.homeDirAbsPath
+import com.an5on.config.ActiveConfiguration.localDirAbsPath
 import com.an5on.error.LocalError
 import com.an5on.error.PunktError
 import com.an5on.states.active.ActiveState
 import com.an5on.states.active.ActiveState.contentEqualsActive
 import com.an5on.states.active.ActiveState.existsInActive
 import com.an5on.states.active.ActiveState.toActivePath
-import com.an5on.states.active.ActiveTransaction
 import com.an5on.states.active.ActiveTransactionCopyToActive
 import com.an5on.states.active.ActiveTransactionMakeDirectories
 import com.an5on.states.local.LocalState
 import com.an5on.states.local.LocalState.contentEqualsLocal
 import com.an5on.states.local.LocalState.existsInLocal
 import com.an5on.states.local.LocalState.toLocalPath
-import com.an5on.states.local.LocalTransaction
 import com.an5on.states.local.LocalTransactionCopyToLocal
 import com.an5on.states.local.LocalTransactionMakeDirectories
 import com.an5on.states.tracked.TrackedEntriesStore
@@ -25,6 +25,10 @@ import com.an5on.states.tracked.TrackedEntryDir
 import com.an5on.states.tracked.TrackedEntryFile
 import com.an5on.utils.Echos
 import com.an5on.utils.FileUtils.getBlake3HashHexString
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.filefilter.FileFilterUtils
+import org.apache.commons.io.filefilter.RegexFileFilter
+import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.attribute.BasicFileAttributes
@@ -66,7 +70,7 @@ object Operations {
             acc
         }
 
-        if (accumulatedActivePaths.any { it.startsWith(ActiveConfiguration.localDirAbsPath) }) {
+        if (accumulatedActivePaths.any { it.startsWith(localDirAbsPath) }) {
             echo.echoWarning(
                 "Directories and files in the local Punkt directory (${ActiveConfiguration.localDirAbsPathname}) will not be synced."
             )
@@ -76,7 +80,7 @@ object Operations {
                     .toMutableSet()
         }
 
-        LocalState.pendingTransactions.addAll(accumulatedActivePaths.fold(mutableSetOf<LocalTransaction>()) { acc, activePath ->
+        LocalState.pendingTransactions.addAll(accumulatedActivePaths.fold(mutableSetOf()) { acc, activePath ->
             if (activePath.existsInLocal().bind()) {
                 if (!activePath.isDirectory() && !activePath.contentEqualsLocal().bind()) {
                     acc.add(LocalTransactionCopyToLocal(activePath))
@@ -101,9 +105,9 @@ object Operations {
     }
 
     fun syncExistingLocal(options: SyncOptions, echo: Echos): Either<PunktError, Unit> = either {
-        val activePathsInLocal = ActiveConfiguration.localDirAbsPath
+        val activePathsInLocal = localDirAbsPath
             .walk(PathWalkOption.BREADTH_FIRST)
-            .filter { it != ActiveConfiguration.localDirAbsPath }
+            .filter { it != localDirAbsPath }
             .map { it.toActivePath().bind() }
             .filter { it.exists() }
             .toSet()
@@ -152,7 +156,7 @@ object Operations {
             acc
         }
 
-        ActiveState.pendingTransactions.addAll(accumulatedLocalPaths.fold(mutableSetOf<ActiveTransaction>()) { acc, localPath ->
+        ActiveState.pendingTransactions.addAll(accumulatedLocalPaths.fold(mutableSetOf()) { acc, localPath ->
             if (localPath.existsInActive().bind()) {
                 if (!localPath.isDirectory() && !localPath.contentEqualsActive().bind()) {
                     acc.add(ActiveTransactionCopyToActive(localPath))
@@ -175,13 +179,13 @@ object Operations {
             LocalError.LocalNotFound()
         }
 
-        val localPathsInLocal = ActiveConfiguration.localDirAbsPath
+        val localPathsInLocal = localDirAbsPath
             .walk(PathWalkOption.BREADTH_FIRST)
-            .filter { it != ActiveConfiguration.localDirAbsPath }
+            .filter { it != localDirAbsPath }
 
 
         ActiveState.pendingTransactions.addAll(
-            localPathsInLocal.fold(mutableSetOf<ActiveTransaction>()) { acc, localPath ->
+            localPathsInLocal.fold(mutableSetOf()) { acc, localPath ->
                 if (localPath.existsInActive().bind()) {
                     if (!localPath.isDirectory() && !localPath.contentEqualsActive().bind()) {
                         acc.add(ActiveTransactionCopyToActive(localPath))
@@ -196,5 +200,89 @@ object Operations {
             })
 
         ActiveState.transact().bind()
+    }
+
+    fun list(activePaths: Set<Path>, options: ListOptions, echo: Echos): Either<PunktError, Unit> = either {
+
+        ensure(LocalState.exists()) {
+            LocalError.LocalNotFound()
+        }
+
+        activePaths.forEach {
+            ensure(it.existsInLocal().bind()) {
+                LocalError.LocalPathNotFound(it)
+            }
+        }
+
+        val includeExcludeFilter = RegexFileFilter(options.include.pattern)
+            .and(RegexFileFilter(options.exclude.pattern).negate())
+            .and(FileFilterUtils.makeCVSAware(null))
+
+        val localPaths = activePaths.map { it.toLocalPath().bind() }.toSet()
+        val accumulatedLocalFiles = localPaths.fold(mutableSetOf<File>()) { acc, localPath ->
+            acc.addAll(FileUtils.listFilesAndDirs(
+                localPath.toFile(),
+                includeExcludeFilter,
+                includeExcludeFilter,
+            ))
+            acc
+        }
+
+        printFiles(accumulatedLocalFiles, options.pathStyle, echo).bind()
+    }
+
+    fun listExistingLocal(options: ListOptions, echos: Echos): Either<PunktError, Unit> = either {
+
+        ensure(LocalState.exists()) {
+            LocalError.LocalNotFound()
+        }
+
+        val includeExcludeFilter = RegexFileFilter(options.include.pattern)
+            .and(RegexFileFilter(options.exclude.pattern).negate())
+            .and(FileFilterUtils.makeCVSAware(null))
+
+        val localFiles = FileUtils.listFilesAndDirs(
+            localDirAbsPath.toFile(),
+            includeExcludeFilter,
+            includeExcludeFilter,
+        )
+
+        printFiles(localFiles, options.pathStyle, echos).bind()
+    }
+
+    private fun printFiles(files: Collection<File>, pathStyle: PathStyles, echos: Echos): Either<PunktError, Unit> = either {
+        when (pathStyle) {
+            PathStyles.ABSOLUTE -> {
+                echos.echo(
+                    files
+                        .map { it.toPath().toActivePath().bind() }
+                        .sorted()
+                        .joinToString("\n"), true, false)
+            }
+
+            PathStyles.RELATIVE -> {
+                echos.echo(
+                    files
+                        .map { it.toPath().toActivePath().bind().relativeTo(homeDirAbsPath) }
+                        .sorted()
+                        .joinToString("\n"), true, false)
+            }
+
+            PathStyles.LOCAL_ABSOLUTE -> {
+                echos.echo(
+                    files
+                        .sorted()
+                        .joinToString("\n"), true, false
+                )
+            }
+
+            PathStyles.LOCAL_RELATIVE -> {
+                echos.echo(
+                    files
+                        .map { it.toPath().relativeTo(localDirAbsPath) }
+                        .sorted()
+                        .joinToString("\n"), true, false)
+            }
+        }
     }
 }
---
+++ src/main/kotlin/operation/PathStyles.kt
@@ -0,0 +1,8 @@
+package com.an5on.operation
+
+enum class PathStyles {
+    ABSOLUTE,
+    RELATIVE,
+    LOCAL_ABSOLUTE,
+    LOCAL_RELATIVE
+}
