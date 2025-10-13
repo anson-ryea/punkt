2bd51d7f953d08b871b467ca8ee71962aae7afe6
Anson Ng <914anson5@gmail.com>
2025-10-14 01:40:35 +0800

    [refactor] update Init command to use new git callers

--- src/main/kotlin/command/Init.kt
+++ src/main/kotlin/command/Init.kt
@@ -1,48 +1,38 @@
 package com.an5on.command
 
 import arrow.core.raise.fold
+import com.an5on.command.options.GlobalOptionGroup
+import com.an5on.command.options.InitOptionGroup
 import com.an5on.config.ActiveConfiguration.config
-import com.an5on.git.GitOperations.cloneRepository
-import com.an5on.git.GitOperations.initialiseRepository
-import com.an5on.git.GitUtils.remoteRepoPatterns
+import com.an5on.git.CloneOperation.clone
+import com.an5on.git.InitOperation.init
+import com.an5on.git.GitRepoPattern.commonPatterns
 import com.an5on.states.local.LocalState
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.Context
 import com.github.ajalt.clikt.core.ProgramResult
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.optional
-import com.github.ajalt.clikt.parameters.options.flag
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.types.int
+import com.github.ajalt.clikt.parameters.groups.provideDelegate
 import io.github.oshai.kotlinlogging.KotlinLogging
 
 /**
  * Initialises a Punkt local repository for storing the clones of the synced dotfiles.
  *
- * @property repo The URL of the remote Punkt repository to clone. If not provided, an empty local repository is initialised at [Configuration.config.general.localPath].
- * Currently, [repo] supports the formats specified in [remoteRepoPatterns].
- * @property ssh A flag indicating whether to use SSH for cloning the remote Punkt repository.
- * @property branch The branch of the remote Punkt repository to clone. If not provided, the default branch is cloned.
- * @property depth The depth for a shallow clone of the remote Punkt repository. If not provided, a full clone is performed.
- * @see remoteRepoPatterns
+ * @property repo The URL of the remote Punkt repository to clone. If not provided, an empty local repository at local state path.
+ * Currently, [repo] supports the formats specified in [commonPatterns].
+ * @property globalOptions The global options for the command, provided by [com.an5on.command.options.GlobalOptionGroup].
+ * @property initOptions The options specific to the init command, provided by [InitOptionGroup].
+ * @see commonPatterns
  * @author Anson Ng <hej@an5on.com>
  * @since 0.1.0
  */
 class Init : CliktCommand() {
+    val globalOptions by GlobalOptionGroup()
+    val initOptions by InitOptionGroup()
     val repo: String? by argument(
         help = "Clone from the specified remote Punkt repository"
     ).optional()
-    val ssh: Boolean? by option(
-        help = "Use SSH for cloning"
-    ).flag()
-    val branch: String? by option(
-        names = arrayOf("-b", "--branch"),
-        help = "Set the branch of the remote Punkt repository to clone"
-    )
-    val depth: Int? by option(
-        names = arrayOf("-p", "--depth"),
-        help = "Clone the remote Punkt repository shallowly with the specified depth"
-    ).int()
 
     override fun help(context: Context) = """
     Initialises a Punkt local repository at ${config.general.localStatePath}.
@@ -52,7 +42,7 @@ class Init : CliktCommand() {
     
     If the repository URL is not complete, Punkt will try to make guesses of it.
     Supported formats for the remote Punkt repository URL:
-    ${remoteRepoPatterns.joinToString("\n") { "- ${it.pattern}" }}
+    ${commonPatterns.joinToString("\n") { "- ${it.pattern}" }}
     
     Examples:
         punkt init
@@ -76,31 +66,20 @@ class Init : CliktCommand() {
             throw ProgramResult(1)
         }
 
-        if (repo == null) {
-            fold(
-                {
-                    initialiseRepository(config.general.localStatePath)
-                },
-                { e ->
-                    logger.error { e.message }
-                    throw ProgramResult(1)
-                }, {
-                    echoSuccess()
-                }
-            )
-        } else {
-            fold(
-                {
-                    cloneRepository(config.general.localStatePath, repo!!, ssh ?: false, branch, depth)
-                },
-                { e ->
-                    logger.error { e.message }
-                    throw ProgramResult(1)
-                }, {
-                    echoSuccess()
-                }
-            )
-        }
+        fold(
+            {
+            if (repo == null) {
+                init(globalOptions.useBundledGit )
+            } else {
+                clone(repo!!, initOptions, globalOptions.useBundledGit)
+            }
+        },
+            { e ->
+                logger.error { e.message }
+                throw ProgramResult(1)
+            }, {
+                echoSuccess()
+            })
     }
 
     private val logger = KotlinLogging.logger {}
