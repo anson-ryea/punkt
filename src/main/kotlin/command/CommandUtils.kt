3663370dd9448297fe9e729c055d08b63c01d273
Anson Ng <914anson5@gmail.com>
2025-10-16 00:01:06 +0800

    [feat] add utility functions for enum handling and indentation

--- src/main/kotlin/command/CommandUtils.kt
+++ src/main/kotlin/command/CommandUtils.kt
@@ -2,7 +2,19 @@ package com.an5on.command
 
 import com.an5on.config.ActiveConfiguration.configuration
 import com.an5on.type.VerbosityType
+import kotlin.enums.enumEntries
 
 object CommandUtils {
+    const val ECHO_CONTENT_INDENTATION = "    " // 4 spaces
     fun determineVerbosity(verbosityOption: VerbosityType?) = verbosityOption ?: configuration.general.verbosity
+
+    inline fun <reified T : Enum<T>> Enum.Companion.toChoices() =
+        enumEntries<T>()
+            .map { it.name.lowercase().replace("_", "-") }
+            .toTypedArray()
+
+    inline fun <reified T : Enum<T>> Enum.Companion.enumEntryOf(choice: String) =
+        enumValueOf<T>(choice.uppercase().replace("-", "_"))
+
+    fun String.indented() = prependIndent(ECHO_CONTENT_INDENTATION)
 }
