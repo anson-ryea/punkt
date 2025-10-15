22650e3c826747ff03b11689a418112cd79d4c97
Anson Ng <914anson5@gmail.com>
2025-10-15 14:40:46 +0800

    [refactor] group options to CommonOptions and GlobalOptions

--- src/main/kotlin/command/options/GlobalOptions.kt
+++ src/main/kotlin/command/options/GlobalOptions.kt
@@ -1,13 +1,15 @@
 package com.an5on.command.options
 
+import com.an5on.operation.PathStyle
 import com.an5on.type.BooleanWithAuto
 import com.an5on.type.VerbosityType
 import com.github.ajalt.clikt.parameters.groups.OptionGroup
 import com.github.ajalt.clikt.parameters.options.convert
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.choice
 
-class GlobalOptionGroup : OptionGroup("Global Options") {
+class GlobalOptions : OptionGroup("Global Options") {
     val useBundledGit by option()
         .choice(
             *BooleanWithAuto.entries
@@ -31,4 +33,16 @@ class GlobalOptionGroup : OptionGroup("Global Options") {
                 it.uppercase().replace("-", "_")
             )
         }
+
+    val pathStyle by option(
+        "-p", "--path-style",
+        help = "Set the path style for displaying the list of managed dotfiles. Options are 'absolute' or 'relative' to the home directory."
+    )
+        .choice(
+            *PathStyle.entries
+                .map { it.name.lowercase().replace("_", "-") }
+                .toTypedArray(),
+        )
+        .convert { PathStyle.valueOf(it.uppercase().replace("-", "_")) }
+        .default(PathStyle.ABSOLUTE)
 }
