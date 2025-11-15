1208b2d362577f340578a0750b3fb8509cb8d2a6
Anson Ng <914anson5@gmail.com>
2025-11-15 16:39:23 +0800

    [doc] enhance KDoc for ActiveState and transaction classes for clarity and accuracy

--- src/main/kotlin/states/active/ActiveState.kt
+++ src/main/kotlin/states/active/ActiveState.kt
@@ -5,19 +5,32 @@ import com.an5on.command.Echos
 import com.an5on.type.Verbosity
 
 /**
- * Manages the files in active state in the Punkt system.
+ * Manages and orchestrates file operations within the "active" state of the Punkt system.
  *
- * This object handles transactions and operations related to the active state, such as copying files and creating directories.
+ * This object serves as a central point for handling transactions that modify files in the active
+ * state, such as copying, updating, or deleting them. It collects proposed changes as a set of
+ * [ActiveTransaction] objects, which can then be reviewed and committed.
  *
- * @author Anson Ng <hej@an5on.com>
  * @since 0.1.0
+ * @author Anson Ng <hej@an5on.com>
  */
 object ActiveState {
     /**
-     * A set of pending transactions to be executed.
+     * A mutable set of [ActiveTransaction] objects waiting to be executed.
+     *
+     * Transactions are added here to be performed later when [commit] is called.
      */
     val pendingTransactions = mutableSetOf<ActiveTransaction>()
 
+    /**
+     * Displays a summary of all pending transactions based on the specified verbosity level.
+     *
+     * This function is intended for providing a dry-run or preview of the changes that will be
+     * made when [commit] is called.
+     *
+     * @param verbosity The verbosity level controlling whether the output is shown.
+     * @param echos The [Echos] instance used for printing messages to the console.
+     */
     fun echoPendingTransactions(verbosity: Verbosity, echos: Echos) {
         echos.echoWithVerbosity(
             "The following operations will be performed:".indented(),
@@ -39,7 +52,10 @@ object ActiveState {
     }
 
     /**
-     * Executes all pending transactions.
+     * Executes all transactions currently held in [pendingTransactions].
+     *
+     * This method iterates through the collected transactions and runs each one, applying the
+     * file system changes. The set of pending transactions is not cleared automatically.
      */
     fun commit() {
         pendingTransactions.forEach {
