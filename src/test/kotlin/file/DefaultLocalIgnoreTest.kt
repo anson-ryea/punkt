01d35a9be902b037dfaae7e9eb29b494081f3b07
Anson Ng <914anson5@gmail.com>
2025-11-18 23:19:58 +0800

    [test] add DefaultLocalIgnoreTest

---
+++ src/test/kotlin/file/DefaultLocalIgnoreTest.kt
@@ -0,0 +1,35 @@
+package file
+
+import com.an5on.config.ActiveConfiguration.configuration
+import com.an5on.config.Configuration
+import com.an5on.config.GlobalConfiguration
+import com.an5on.file.DefaultLocalIgnore
+import java.nio.file.Files
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class DefaultLocalIgnoreTest {
+    private val testConfiguration = Configuration(
+        GlobalConfiguration(
+            localStatePath = Files.createTempDirectory("test-share"),
+            activeStatePath = Files.createTempDirectory("test-home"),
+        )
+    )
+
+    init {
+        configuration = testConfiguration
+    }
+
+    @Test
+    fun matchesWithDefaultLocalIgnore() {
+        assertTrue {
+            DefaultLocalIgnore.ignorePathMatchers.any {
+                it.matches(
+                    configuration.global.localStatePath.resolve(
+                        ".gitignore"
+                    )
+                )
+            }
+        }
+    }
+}
