d35e9a67c081f4d1421a9e873792860e2e6f1445
Anson Ng <914anson5@gmail.com>
2025-11-22 11:19:19 +0800

    [fix] update Activate command description to clarify file copying instead of linking

--- src/main/kotlin/command/Activate.kt
+++ src/main/kotlin/command/Activate.kt
@@ -12,7 +12,7 @@ import com.github.ajalt.clikt.parameters.types.path
 /**
  * A command to activate files, linking them from the local repository to their destination in the filesystem.
  *
- * This command takes a list of target files or directories and creates symbolic links to them from the `punkt`
+ * This command takes a list of target files or directories and copy them from the `punkt`
  * local state to the user's filesystem, effectively "activating" them. If no targets are specified, it can be
  * configured to activate all tracked files.
  *
