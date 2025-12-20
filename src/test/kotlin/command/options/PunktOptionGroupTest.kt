491897f8ead970a8c8000ed3e7cac06115387202
Anson Ng <914anson5@gmail.com>
2025-12-20 09:08:55 +0800

    [test] add missing package path to PunktOptionGroupTest

--- src/test/kotlin/command/options/PunktOptionGroupTest.kt
+++ src/test/kotlin/command/options/PunktOptionGroupTest.kt
@@ -1,9 +1,11 @@
+package command.options
+
 import com.an5on.command.options.PunktOptionGroup
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 // sample enum for tests
-//private enum class SampleEnum { FIRSTOption, SECOND_OPTION, THIRD_OPTION }
+// private enum class SampleEnum { FIRSTOption, SECOND_OPTION, THIRD_OPTION }
 private enum class SampleEnum {  SECOND_OPTION, THIRD_OPTION }
 
 // tests
@@ -11,7 +13,7 @@ class PunktOptionGroupTest: PunktOptionGroup("Test Options") {
 
     @Test
     fun toChoicesTest() {
-        val choices = Enum.Companion.toChoices<SampleEnum>()
+        val choices = Enum.toChoices<SampleEnum>()
         // expected order matches enum declaration
 //        val expected = arrayOf("firstoption", "second-option", "third-option")
         val expected = arrayOf("second-option", "third-option")
@@ -21,8 +23,8 @@ class PunktOptionGroupTest: PunktOptionGroup("Test Options") {
     @Test
     fun enumEntryOfTest() {
 //        val e1 = Enum.Companion.enumEntryOf<SampleEnum>("firstoption")
-        val e2 = Enum.Companion.enumEntryOf<SampleEnum>("second-option")
-        val e3 = Enum.Companion.enumEntryOf<SampleEnum>("third-option")
+        val e2 = Enum.enumEntryOf<SampleEnum>("second-option")
+        val e3 = Enum.enumEntryOf<SampleEnum>("third-option")
 //        should fix
 //        assertEquals(SampleEnum.FIRSTOption, e1)
         assertEquals(SampleEnum.SECOND_OPTION, e2)
@@ -31,8 +33,8 @@ class PunktOptionGroupTest: PunktOptionGroup("Test Options") {
 
     @Test
     fun roundTripForToChoicesToEnumsToChoices() {
-        val choices = Enum.Companion.toChoices<SampleEnum>()
-        val roundTripped = choices.map { Enum.Companion.enumEntryOf<SampleEnum>(it) }
+        val choices = Enum.toChoices<SampleEnum>()
+        val roundTripped = choices.map { Enum.enumEntryOf<SampleEnum>(it) }
             .map { it.name.lowercase().replace("_", "-") }
             .toTypedArray()
         assertArrayEquals(choices, roundTripped)
